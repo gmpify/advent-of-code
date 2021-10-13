5425cfcb5ac4cf5bdebd658bbad33d59b130a596
Gabriel Parreiras <gab.parreiras@gmail.com>
2021-10-13 22:18:47 +0100

    Make 2015.01 OOP and add Part 2

--- 2015/01/app.py
+++ 2015/01/app.py
@@ -1,15 +1,40 @@
-def what_floor(instructions):
-    floor = 0
-    for instruction in instructions:
-        if instruction == '(':
-            floor += 1
-        elif instruction == ')':
-            floor -= 1
+class Santa:
+    MOVE_UP = '('
+    MOVE_DOWN = ')'
+
+    def __init__(self, instructions) -> None:
+        self._instructions = instructions
+        self.step = 0
+        self.floor = 0
+
+    def follow_instructions(self):
+        while self.can_move():
+            self.move()
+
+    def enter_basement(self):
+        while self.can_move() and self.floor >= 0:
+            self.move()
+
+    def can_move(self):
+        return self.step < len(self._instructions)
+
+    def move(self):
+        instruction = self._instructions[self.step]
+        if instruction == self.MOVE_UP:
+            self.floor += 1
+        elif instruction == self.MOVE_DOWN:
+            self.floor -= 1
         else:
-            raise Exception('Instruction not either "(" or ")"')
-    return floor
+            raise Exception('Wrong instructions')
+        self.step += 1
 
 if __name__ == '__main__':
-    f = open('input.txt', 'r')
-    input = f.read()
-    print(what_floor(input))
+    f = open('input_part1.txt', 'r')
+    instructions = f.read()
+    santa = Santa(instructions)
+    santa.follow_instructions()
+    print('Part 1: ', santa.floor)
+
+    santa = Santa(instructions)
+    santa.enter_basement()
+    print('Part 2: ', santa.step)
--- 2015/01/test_app.py
+++ 2015/01/test_app.py
@@ -2,22 +2,61 @@ import pytest
 import app
 
 def test_matching_instructions():
-    assert app.what_floor('(())') == 0
-    assert app.what_floor('()()') == 0
+    s = app.Santa('(())')
+    s.follow_instructions()
+    assert s.floor == 0
+
+    s = app.Santa('()()')
+    s.follow_instructions()
+    assert s.floor == 0
+
 
 def test_upper_floors():
-    assert app.what_floor('(((') == 3
-    assert app.what_floor('(()(()(') == 3
-    assert app.what_floor('))(((((') == 3
+    s = app.Santa('(((')
+    s.follow_instructions()
+    assert s.floor == 3
+
+    s = app.Santa('(()(()(')
+    s.follow_instructions()
+    assert s.floor == 3
+
+    s = app.Santa('))(((((')
+    s.follow_instructions()
+    assert s.floor == 3
+
 
 def test_basement_floors():
-    assert app.what_floor('())') == -1
-    assert app.what_floor('))(') == -1
+    s = app.Santa('())')
+    s.follow_instructions()
+    assert s.floor == -1
+
+    s = app.Santa('))(')
+    s.follow_instructions()
+    assert s.floor == -1
+
 
 def test_deeper_basement_floors():
-    assert app.what_floor(')))') == -3
-    assert app.what_floor(')())())') == -3
+    s = app.Santa(')))')
+    s.follow_instructions()
+    assert s.floor == -3
+
+    s = app.Santa(')())())')
+    s.follow_instructions()
+    assert s.floor == -3
+
 
 def test_invalid_instructions():
     with pytest.raises(Exception):
-        app.what_floor('((foobar))')
+        app.Santa('((foobar))').follow_instructions()
+
+
+def test_enter_basement_on_first_step():
+    s = app.Santa(')')
+    s.enter_basement()
+    assert s.step == 1
+
+
+def test_enter_basement_on_last_step():
+    s = app.Santa('()())')
+    s.enter_basement()
+    assert s.step == 5
