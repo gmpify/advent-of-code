e2a0fa25b8a2b6e934229b632dcbdec57ffc70e2
Gabriel Parreiras <gab.parreiras@gmail.com>
2021-10-14 21:38:18 +0100

    2015.02: Remove instructions from instantiation

--- 2015/03/app.py
+++ 2015/03/app.py
@@ -4,44 +4,46 @@ class Santa:
     EAST_MOVE = '>'
     WEST_MOVE = '<'
 
-    def __init__(self, instructions) -> None:
-        self.instructions = instructions
+    def __init__(self) -> None:
         self.x = 0
         self.y = 0
         self.presents_delivered_per_house = {}
         self.deliver_present()
 
-    def move(self):
-        for instruction in self.instructions:
-            if instruction == self.NORTH_MOVE:
-                self.y += 1
-                self.deliver_present()
-            elif instruction == self.SOUTH_MOVE:
-                self.y -= 1
-                self.deliver_present()
-            elif instruction == self.EAST_MOVE:
-                self.x += 1
-                self.deliver_present()
-            elif instruction == self.WEST_MOVE:
-                self.x -= 1
-                self.deliver_present()
-            else:
-                raise Exception('Wrong instructions')
-
     def houses_visited(self):
         return len(self.presents_delivered_per_house)
 
-    def position(self):
-        return (self.x, self.y)
+    def move(self, instructions):
+        for instruction in instructions:
+            self.step(instruction)
+
+    def step(self, instruction):
+        if instruction == self.NORTH_MOVE:
+            self.y += 1
+            self.deliver_present()
+        elif instruction == self.SOUTH_MOVE:
+            self.y -= 1
+            self.deliver_present()
+        elif instruction == self.EAST_MOVE:
+            self.x += 1
+            self.deliver_present()
+        elif instruction == self.WEST_MOVE:
+            self.x -= 1
+            self.deliver_present()
+        else:
+            raise Exception('Wrong instructions')
 
     def deliver_present(self):
         self.presents_delivered_per_house[self.position()] = self.presents_delivered_per_house.get(self.position(), 0) + 1
+    
+    def position(self):
+        return (self.x, self.y)
 
 if __name__ == '__main__':
     f = open('input.txt', 'r')
     input = f.read()
 
-    santa = Santa(input)
-    santa.move()
+    santa = Santa()
+    santa.move(input)
 
     print('Houses visited: ', santa.houses_visited())
