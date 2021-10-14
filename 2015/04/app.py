3737580f594c43f9ed7aea02dbb8a1bcbb2849b7
Gabriel Parreiras <gab.parreiras@gmail.com>
2021-10-14 22:41:44 +0100

    2015.04: Add solution for parts 1 and 2

--- 2015/04/app.py
+++ 2015/04/app.py
@@ -1,15 +1,22 @@
 import hashlib
+import re
 
 class Md5Miner:
     def __init__(self, secret_key):
         self.secret_key = secret_key
         self.answer = 0
 
-    def mine(self):
+    def mine(self, zeros = 5):
         while True:
             current_key = self.secret_key + str(self.answer)
             md5 = hashlib.md5(current_key.encode())
             md5_hash = md5.hexdigest()
-            if md5_hash[0:5] == '00000':
+            if re.match(rf"0{{{zeros}}}", md5_hash):
                 return self.answer
             self.answer += 1
+
+if __name__ == '__main__':
+    miner = Md5Miner('ckczppom')
+    print('The answer is: ', miner.mine())
+
+    print('The second part answer is: ', miner.mine(6))
