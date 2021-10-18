12203afe1f254cdd20d05aa62114ced139f5cf11
Gabriel Parreiras <gab.parreiras@gmail.com>
2021-10-19 00:51:51 +0100

    2015.07: Set signal from within the Wire class

--- 2015/07/app.py
+++ 2015/07/app.py
@@ -42,48 +42,55 @@ class WireValue(Wire):
     def resolve(self):
         m = re.match(r'(?P<value>\d+)', self.signal_input)
         value = m.group('value')
-        return lambda _: int(value)
+        self.signal = int(value)
+        return self.signal
 
 class WirePassThrough(Wire):
     def resolve(self):
         m = re.match(r'(?P<wire>\w{1,2})', self.signal_input)
         wire = m.group('wire')
-        return lambda wires: wires[wire].signal
+        self.signal = self.resolved_dependencies[wire]
+        return self.signal
 
 class WireAnd(Wire):
     def resolve(self):
         m = re.match(r'(?P<wire_1>[a-z1-9]{1,2}) AND (?P<wire_2>[a-z]{1,2})', self.signal_input)
         wire_1, wire_2 = m.group('wire_1'), m.group('wire_2')
-        result = lambda _: None
+        result = None
         if wire_1.isnumeric():
-            result = lambda wires: int(wire_1) & wires[wire_2].signal
+            result = int(wire_1) & self.resolved_dependencies[wire_2]
         else:
-            result = lambda wires: wires[wire_1].signal & wires[wire_2].signal
-        return result
+            result = self.resolved_dependencies[wire_1] & self.resolved_dependencies[wire_2]
+        self.signal = result
+        return self.signal
 
 class WireOr(Wire):
     def resolve(self):
         m = re.match(r'(?P<wire_1>[a-z]{1,2}) OR (?P<wire_2>[a-z]{1,2})', self.signal_input)
         wire_1, wire_2 = m.group('wire_1'), m.group('wire_2')
-        return lambda wires: wires[wire_1].signal | wires[wire_2].signal
+        self.signal = self.resolved_dependencies[wire_1] | self.resolved_dependencies[wire_2]
+        return self.signal
 
 class WireNot(Wire):
     def resolve(self):
         m = re.match(r'NOT (?P<wire>[a-z]{1,2})', self.signal_input)
         wire = m.group('wire')
-        return lambda wires: ~ wires[wire].signal
+        self.signal = ~ self.resolved_dependencies[wire]
+        return self.signal
 
 class WireLShift(Wire):
     def resolve(self):
         m = re.match(r'(?P<wire>[a-z]{1,2}) LSHIFT (?P<bit_position>\d+)', self.signal_input)
         wire, bit_position = m.group('wire'), m.group('bit_position')
-        return lambda wires: wires[wire].signal << int(bit_position)
+        self.signal = self.resolved_dependencies[wire] << int(bit_position)
+        return self.signal
 
 class WireRShift(Wire):
     def resolve(self):
         m = re.match(r'(?P<wire>[a-z]{1,2}) RSHIFT (?P<bit_position>\d+)', self.signal_input)
         wire, bit_position = m.group('wire'), m.group('bit_position')
-        return lambda wires: wires[wire].signal >> int(bit_position)
+        self.signal = self.resolved_dependencies[wire] >> int(bit_position)
+        return self.signal
 
 class Circuit:
     def __init__(self):
@@ -94,9 +101,7 @@ class Circuit:
             element = self.find_unresolved_element_without_dependencies()
             if element is None:
                 break
-            func = self.wires[element].resolve()
-            value = func(self.wires)
-            self.wires[element].signal = value
+            value = self.wires[element].resolve()
             print(f'{element}: {value}')
             self.resolve_dependencies_from_element(element, value)
 
