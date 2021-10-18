5c750516dc8385aedccf138094267c9e58a06640
Gabriel Parreiras <gab.parreiras@gmail.com>
2021-10-18 13:27:31 +0100

    2015.06: Add classes for solution

---
+++ 2015/06/app.py
@@ -0,0 +1,73 @@
+import re
+
+class Light:
+    OFF = 0
+    ON = 1
+    def __init__(self):
+        self.status = self.OFF
+
+    def command(self, action):
+        str_to_func = {
+            'turn on': self.turn_on,
+            'turn off': self.turn_off,
+            'toggle': self.toggle
+        }
+        func = str_to_func.get(action, lambda: None)
+        return func()
+
+    def turn_on(self):
+        self.status = self.ON
+
+    def turn_off(self):
+        self.status = self.OFF
+
+    def toggle(self):
+        if self.status == self.ON:
+            self.turn_off()
+        else:
+            self.turn_on()
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        if (isinstance(other, Point)):
+            return self.x == other.x and self.y == other.y
+        return False
+
+    @classmethod
+    def parse(self, coordinates):
+        x, y = [int(n) for n in coordinates.split(',')]
+        return Point(x, y)
+
+
+class Grid:
+    def __init__(self, size=1000):
+        self.size = 1000
+        self.lights = [[Light() for _ in range(size)] for _ in range(size)]
+
+    def count_lights(self, status=Light.ON):
+        result = 0
+        for x in range(self.size):
+            for y in range(self.size):
+                light = self.lights[x][y]
+                if light.status == status:
+                    result += 1
+        return result
+
+    def run_command(self, command):
+        action, begin, end = self.parse_command(command)
+        for x in range(begin.x, end.x + 1):
+            for y in range(begin.y, end.y + 1):
+                light = self.lights[x][y]
+                light.command(action)
+
+    def parse_command(self, command):
+        m = re.match(r'(?P<action>.*)\s(?P<begin>\d{1,3},\d{1,3}) through (?P<end>\d{1,3},\d{1,3})', command)
+        action = m.group('action')
+        begin = Point.parse(m.group('begin'))
+        end = Point.parse(m.group('end'))
+        return action, begin, end
---
+++ 2015/06/test_app.py
@@ -0,0 +1,64 @@
+import pytest
+from app import *
+
+
+@pytest.fixture
+def light():
+    return Light()
+
+
+def test_light_turn_on_and_off(light):
+    assert light.status == Light.OFF
+    light.turn_on()
+    assert light.status == Light.ON
+    light.turn_off()
+    assert light.status == Light.OFF
+
+
+def test_light_toggle(light):
+    assert light.status == Light.OFF
+    light.toggle()
+    assert light.status == Light.ON
+    light.toggle()
+    assert light.status == Light.OFF
+
+
+def test_light_command(light):
+    assert light.status == Light.OFF
+    light.command('turn on')
+    assert light.status == Light.ON
+    light.command('turn off')
+    assert light.status == Light.OFF
+    light.command('toggle')
+    assert light.status == Light.ON
+
+
+def test_point():
+    p = Point(1, 2)
+    assert p.x == 1
+    assert p.y == 2
+
+
+def test_point_parse():
+    point = Point.parse('0,1')
+    assert point == Point(0, 1)
+
+
+def test_grid_count():
+    grid = Grid()
+    assert grid.count_lights() == 0
+    assert grid.count_lights(Light.OFF) == 1_000_000
+
+
+def test_grid_parse_command():
+    grid = Grid()
+    assert grid.parse_command('turn on 0,0 through 999,999') == ('turn on', Point(0,0), Point(999,999))
+    assert grid.parse_command('toggle 0,0 through 999,0') == ('toggle', Point(0, 0), Point(999, 0))
+    assert grid.parse_command('turn off 499,499 through 500,500') == ('turn off', Point(499, 499), Point(500, 500))
+
+
+def test_grid_run_command():
+    grid = Grid()
+    assert grid.count_lights() == 0
+    grid.run_command('turn on 0,0 through 999,999')
+    assert grid.count_lights() == 1_000_000
