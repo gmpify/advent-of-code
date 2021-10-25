b0bed6e2c5e8713f201e1d29719e10698ba9e47d
Gabriel Parreiras <gab.parreiras@gmail.com>
2021-10-25 23:27:19 +0100

    2015.08: Add solution for Part Two

--- 2015/08/app.py
+++ 2015/08/app.py
@@ -1,18 +1,27 @@
+def get_encoded_length(line):
+    srting = line.decode('utf-8')
+    string = repr(srting)[1:-1]
+    string = '"' + string.replace('"', '\\"') + '"'
+    return len(string)
+
 def get_literal_length(line):
     return len(line)
 
 def get_string_length(line):
-    return len(eval(line.decode('utf-8'))) 
+    string = line.decode('utf-8')
+    return len(eval(string))
 
 def process_file(file_name):
-    total_string_literals = total_string = 0
+    total_string_literals = total_string = total_string_encoded = 0
     for line in open(file_name, 'rb'):
         line = line.strip()
         total_string_literals += get_literal_length(line)
         total_string += get_string_length(line)
-    return (total_string_literals, total_string)
+        total_string_encoded += get_encoded_length(line)
+    return (total_string_literals, total_string, total_string_encoded)
 
 if __name__ == '__main__':
-    total_string_literals, total_string = process_file('input.txt')
+    total_string_literals, total_string, total_string_encoded = process_file('input.txt')
 
     print('Solution 1: ', total_string_literals - total_string)
+    print('Solution 2: ', total_string_encoded - total_string_literals)
--- 2015/08/test_app.py
+++ 2015/08/test_app.py
@@ -16,6 +16,7 @@ def line0(test_file):
 def test_line0(line0):
     assert get_literal_length(line0) == 2
     assert get_string_length(line0) == 0
+    assert get_encoded_length(line0) == 6
 
 
 @pytest.fixture
@@ -26,6 +27,7 @@ def line1(test_file):
 def test_line1(line1):
     assert get_literal_length(line1) == 5
     assert get_string_length(line1) == 3
+    assert get_encoded_length(line1) == 9
 
 
 @pytest.fixture
@@ -36,6 +38,7 @@ def line2(test_file):
 def test_line2(line2):
     assert get_literal_length(line2) == 10
     assert get_string_length(line2) == 7
+    assert get_encoded_length(line2) == 16
 
 
 @pytest.fixture
@@ -46,9 +49,11 @@ def line3(test_file):
 def test_line3(line3):
     assert get_literal_length(line3) == 6
     assert get_string_length(line3) == 1
+    assert get_encoded_length(line3) == 11
 
 
 def test_process_file():
-    total_string_literals, total_string = process_file('test.txt')
+    total_string_literals, total_string, total_string_encoded = process_file('test.txt')
     assert total_string_literals == 23
     assert total_string == 11
+    assert total_string_encoded == 42
