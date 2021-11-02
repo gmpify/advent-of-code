3f105c9a580af104251b77ab9ae96139adc654a6
Gabriel Parreiras <gab.parreiras@gmail.com>
2021-11-02 21:46:15 +0000

    2015.12: Add Part Two

--- 2015/12/app.py
+++ 2015/12/app.py
@@ -1,17 +1,41 @@
 import re
+import json
 
 class JsonParser:
     def __init__(self, content):
-        self.content = content
+        self.content = json.loads(content)
         self.nums = []
-        self.parse_nums()
+        self.parse(self.content)
 
-    def parse_nums(self):
-        matches = re.findall(r'-?\d+', self.content)
-        self.nums = [int(m) for m in matches]
+    def parse(self, content):
+        if isinstance(content, int):
+            self.parse_int(content)
+        elif isinstance(content, list):
+            self.parse_list(content)
+        elif isinstance(content, dict):
+            self.parse_dict(content)
+
+    def parse_int(self, content):
+        self.nums.append(content)
+
+    def parse_list(self, content):
+        for elem in content:
+            self.parse(elem)
+
+    def parse_dict(self, content):
+        for elem_key, elem_value in content.items():
+            self.parse(elem_key)
+            self.parse(elem_value)
 
     def sum_all_nums(self):
         return sum(self.nums)
+
+
+class JsonParserRed(JsonParser):
+    def parse_dict(self, content):
+        if 'red' not in content.values():
+            super().parse_dict(content)
+
 
 if __name__ == '__main__':
     f = open('input.txt')
@@ -21,3 +45,8 @@ if __name__ == '__main__':
     jp_sum = jp.sum_all_nums()
 
     print('Part One:', jp_sum)
+
+    jp_red = JsonParserRed(content)
+    jp_sum_red = jp_red.sum_all_nums()
+
+    print('Part Two:', jp_sum_red)
--- 2015/12/test_app.py
+++ 2015/12/test_app.py
@@ -18,3 +18,17 @@ def test_sum_all_nums():
 
     jp = JsonParser('[-1,{"a":1}]')
     assert jp.sum_all_nums() == 0
+
+
+def test_sum_all_nums_red():
+    jp = JsonParserRed('[1,2,3]')
+    assert jp.sum_all_nums() == 6
+
+    jp = JsonParserRed('[1,{"c":"red","b":2},3]')
+    assert jp.sum_all_nums() == 4
+
+    jp = JsonParserRed('{"d":"red","e":[1,2,3,4],"f":5}')
+    assert jp.sum_all_nums() == 0
+
+    jp = JsonParserRed('[1,"red",5]')
+    assert jp.sum_all_nums() == 6
