59a751f26e5592a5f785bcc7af10d76ed05c350c
Gabriel Parreiras <gab.parreiras@gmail.com>
2022-07-22 09:14:03 +0100

    2015.17: Refactor

--- 2015/17/app.py
+++ 2015/17/app.py
@@ -7,19 +7,22 @@ def find_combinations_n(total_liters, containers, n):
             result.append(c)
     return result
 
-def find_combinations(total_liters, containers, only_minimum=False):
-    result = []
+def find_combinations(total_liters, containers):
+    result = {}
 
     containers.sort(reverse=True)
     for n in range(len(containers) + 1):
-        result.extend(find_combinations_n(total_liters, containers, n))
-        if only_minimum and len(result) > 0:
-            return result
+        c = find_combinations_n(total_liters, containers, n)
+        if len(c) > 0:
+            result[n] = c
 
     return result
 
 if __name__ == '__main__':
     containers = [int(l) for l in open('input.txt')]
-    print(f"Part 1: {len(find_combinations(150, containers))} combinations")
+    result = find_combinations(150, containers)
 
-    print(f"Part 2: {len(find_combinations(150, containers, True))} combinations")
+    print(f"Part 1: {sum(map(lambda x: len(x), result.values()))} combinations")
+
+    m = min(result.keys())
+    print(f"Part 2: {len(result[m])} combinations")
