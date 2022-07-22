1d111ec6c33a41e5afcd04803ab057f8ae875f06
Gabriel Parreiras <gab.parreiras@gmail.com>
2022-07-22 09:06:57 +0100

    2015.17: Add Part Two

--- 2015/17/app.py
+++ 2015/17/app.py
@@ -5,18 +5,21 @@ def find_combinations_n(total_liters, containers, n):
     for c in combinations(containers, n):
         if sum(c) == total_liters:
             result.append(c)
-        elif sum(c) < total_liters:
-            break
     return result
 
-def find_combinations(total_liters, containers):
+def find_combinations(total_liters, containers, only_minimum=False):
     result = []
 
     containers.sort(reverse=True)
-    for n in range(len(containers)):
+    for n in range(len(containers) + 1):
         result.extend(find_combinations_n(total_liters, containers, n))
+        if only_minimum and len(result) > 0:
+            return result
 
     return result
 
 if __name__ == '__main__':
-    pass
+    containers = [int(l) for l in open('input.txt')]
+    print(f"Part 1: {len(find_combinations(150, containers))} combinations")
+
+    print(f"Part 2: {len(find_combinations(150, containers, True))} combinations")
